daf9e3948d9f788b2d832d24a3743a65f5e5967d
Hamza <said@MacBook-Air-de-Said.local>
2024-11-27 01:09:17 +0100

    Initial commit

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * The path to the "home" route for your application.
+     *
+     * This is used by Laravel authentication to redirect users after login.
+     *
+     * @var string
+     */
+    public const HOME = '/home';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        // You can add additional route model bindings or pattern filters here
+    }
+
+    /**
+     * Define the routes for your application.
+     *
+     * @return void
+     */
+    public function map()
+    {
+        $this->mapWebRoutes();
+        $this->mapApiRoutes();
+        // You can add other custom route mappings here if needed
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * These routes all have the "web" middleware group.
+     *
+     * @return void
+     */
+    protected function mapWebRoutes()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/web.php'));
+    }
+
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless and use the "api" middleware group.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes()
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/api.php'));
+    }
+}
